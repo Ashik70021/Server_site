59fb34edba53a693cda1e3a964b9fde9f12c9226
Ashik70021 <ashik70021@gmail.com>
2024-12-30 20:13:20 +0600

    create server site with express cors mongodb dotenv

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const cors = require("cors");
+const { MongoClient } = require('mongodb');
+const { ServerApiVersion } = require("mongodb");
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// mongodb
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpk7gxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url,{
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true 
+    }
+});
+
+const dbConnet = async () =>{
+  try{
+    client.connect();
+    console.log("Database connected Successfully");
+  }
+  catch(error){
+    console.log(error.name, error.message);
+  }
+};
+
+dbConnet()
+
+// api
+app.get("/",(req,res)=>{
+  res.send("Server is running");
+});
+app.listen(port, ()=>{
+  console.log(`Server is running on port, ${port}`);
+});
